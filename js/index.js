e2d83b1b1749e66c3271453c6464ea83ad30a760
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-10-14 21:52:08 +0600

    donated section title updated

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.getElementById("donation-btn").addEventListener("click", function () {
 document.getElementById("history-btn").addEventListener("click", function () {
   showBgById("history-btn");
 });
-function donatedButtonId(amountid, balanceid, donatedid) {
+function donatedButtonId(amountid, balanceid, donatedid, titleid) {
   const donationAmount = InputValueById(amountid);
   const donationBalance = InputTextById(balanceid);
 
@@ -28,7 +28,7 @@ function donatedButtonId(amountid, balanceid, donatedid) {
   document.getElementById(balanceid).innerText = updateBalance;
   document.getElementById(donatedid).innerText = donatedMoney;
   //   transaction updated
-  const title = document.getElementById("flood-title").innerText;
+  const title = document.getElementById(titleid).innerText;
   const p = document.createElement("p");
   p.innerText = `${donatedMoney} Taka is donated for flood at ${title}`;
   p.style.fontWeight = "semi-bold";
@@ -39,20 +39,20 @@ function donatedButtonId(amountid, balanceid, donatedid) {
   document.getElementById("history-container").appendChild(h6);
 }
 document.getElementById("donate-btn1").addEventListener("click", function () {
-  donatedButtonId("donation-amount1", "donation-balance", "donated-balance1");
+  donatedButtonId("donation-amount1", "donation-balance", "donated-balance1", "donate-title1");
 });
 document.getElementById("donate-btn2").addEventListener("click", function () {
-  donatedButtonId("donation-amount2", "donation-balance", "donated-balance2");
+  donatedButtonId("donation-amount2", "donation-balance", "donated-balance2", "donate-title2");
 });
 document.getElementById("donate-btn3").addEventListener("click", function () {
-  donatedButtonId("donation-amount3", "donation-balance", "donated-balance3");
+  donatedButtonId("donation-amount3", "donation-balance", "donated-balance3", "donate-title3");
 });
 document.getElementById("donate-btn4").addEventListener("click", function () {
-  donatedButtonId("donation-amount4", "donation-balance", "donated-balance4");
+  donatedButtonId("donation-amount4", "donation-balance", "donated-balance4", "donate-title4");
 });
 document.getElementById("donate-btn5").addEventListener("click", function () {
-  donatedButtonId("donation-amount5", "donation-balance", "donated-balance5");
+  donatedButtonId("donation-amount5", "donation-balance", "donated-balance5", "donate-title5");
 });
 document.getElementById("donate-btn6").addEventListener("click", function () {
-  donatedButtonId("donation-amount6", "donation-balance", "donated-balance6");
+  donatedButtonId("donation-amount6", "donation-balance", "donated-balance6", "donate-title6");
 });
