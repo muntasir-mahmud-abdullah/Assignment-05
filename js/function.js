c5b9e1cdc82eb500a619e603b416fbcb957cc334
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-10-15 11:47:24 +0600

    blog page functionality added

--- js/function.js
+++ js/function.js
@@ -1,20 +1,43 @@
-document.getElementById ('donation-btn').addEventListener('click',function(){
-    document.getElementById('donation-container').classList.remove('hidden');
-    document.getElementById('history-container').classList.add('hidden');
-})
-document.getElementById ('history-btn').addEventListener('click',function(){
-    document.getElementById('history-container').classList.remove('hidden');
-    document.getElementById('donation-container').classList.add('hidden');
-})
-function showBgById(id){
-    document.getElementById('donation-btn').classList.remove('bg-green-300','border-none');
-    document.getElementById('history-btn').classList.remove('bg-green-300','border-none');
-    document.getElementById(id).classList.add('bg-green-300','border-none');
-};
-
-function InputValueById(id){
-    return parseInt(document.getElementById(id).value);
+document.getElementById("donation-btn").addEventListener("click", function () {
+  document.getElementById("donation-container").classList.remove("hidden");
+  document.getElementById("history-container").classList.add("hidden");
+  document.getElementById("blog-container").classList.add("hidden");
+  document.getElementById("blog-btn").classList.remove("hidden");
+  document.getElementById("donation-balance").classList.remove("hidden");
+  document.getElementById("donation-text").classList.remove("hidden");
+  document.getElementById("donation-logo").classList.remove("hidden");
+});
+document.getElementById("history-btn").addEventListener("click", function () {
+  document.getElementById("history-container").classList.remove("hidden");
+  document.getElementById("donation-container").classList.add("hidden");
+  document.getElementById("blog-container").classList.add("hidden");
+  document.getElementById("blog-btn").classList.remove("hidden");
+  document.getElementById("donation-balance").classList.remove("hidden");
+  document.getElementById("donation-text").classList.remove("hidden");
+  document.getElementById("donation-logo").classList.remove("hidden");
+});
+document.getElementById("blog-btn").addEventListener("click", function () {
+  document.getElementById("blog-container").classList.remove("hidden");
+  document.getElementById("donation-container").classList.add("hidden");
+  document.getElementById("history-container").classList.add("hidden");
+  document.getElementById("blog-btn").classList.add("hidden");
+  document.getElementById("donation-balance").classList.add("hidden");
+  document.getElementById("donation-text").classList.add("hidden");
+  document.getElementById("donation-logo").classList.add("hidden");
+});
+function showBgById(id) {
+  document
+    .getElementById("donation-btn")
+    .classList.remove("bg-green-300", "border-none");
+  document
+    .getElementById("history-btn")
+    .classList.remove("bg-green-300", "border-none");
+  document.getElementById(id).classList.add("bg-green-300", "border-none");
 }
-function InputTextById(id){
-    return parseInt(document.getElementById(id).innerText);
+
+function InputValueById(id) {
+  return parseInt(document.getElementById(id).value);
+}
+function InputTextById(id) {
+  return parseInt(document.getElementById(id).innerText);
 }
